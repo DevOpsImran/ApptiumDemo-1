8c9b8fc039eea60faffe1d88a914c33edac56bb6
DevOpsImran <41373547+DevOpsImran@users.noreply.github.com>
2021-04-21 13:28:33 +0530

    Delete fe-nonprod.vars.tf

--- fe-nonprod.vars.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-     default = "AKIASIP3JHGR2MSPAYCG"
-}
-variable "secret_key" {
-     default = "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"
-}
-variable "aws_region" {
-	default = "ca-central-1"
-}
