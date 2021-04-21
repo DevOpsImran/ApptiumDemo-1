1707a24bc2be10d8613b7aeda12977c5c8b5e8b1
DevOpsImran <41373547+DevOpsImran@users.noreply.github.com>
2021-04-21 13:29:14 +0530

    Update fe-nonprod.vars.tf

--- fe-nonprod.vars.tf
+++ fe-nonprod.vars.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-     default = "AKIASIP3JHGR2MSPAYCG"
+     default = "AKIASIP3JHGR2MS"
 }
 variable "secret_key" {
-     default = "ef3s9letyr22XSPHmzQf/0gtJnZiWF+9ecW2F9cZ"
+     default = "ef3s9letyr22XSPHmzQf/tJnZiWF+9ecW2F9cZ"
 }
 variable "aws_region" {
 	default = "ca-central-1"
